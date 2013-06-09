52da370cb20aa07cd847bb1554da750c9552d38a
SOMTD <matsuda-so@kayac.com>
2013-06-09 20:55:11 +0900

    first commit

---
+++ HelloKit/Classes/HelloKit.m
@@ -0,0 +1,17 @@
+//
+//  HelloKit.m
+//  HelloKit
+//
+//  Created by SOMTD on 2013/06/09.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import "HelloKit.h"
+
+@implementation HelloKit
+
+- (NSString *)sayHello {
+   return @"Hello!!";
+}
+
+@end
---
+++ HelloKit/Headers/HelloKit.h
@@ -0,0 +1,15 @@
+//
+//  HelloKit.h
+//  HelloKit
+//
+//  Created by SOMTD on 2013/06/09.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloKit : NSObject
+
+- (NSString *)sayHello;
+
+@end
